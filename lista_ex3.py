40185a196c67b781147d67e97ced08d0ec94de30
HenriqueCruz1 <henrique.almeida96@outlook.com>
2026-03-01 23:15:23 -0300

    Adicionando exercicio de inserção ordenada

--- lista_ex3.py
+++ lista_ex3.py
@@ -6,7 +6,20 @@ numeros = []
 
 for c in range (5):
     numero = int(input('Digite um numero. '))
-    numeros.append(numero)
 
-print (numeros)
+    if not numeros or numero > numeros[-1]: # se o contador estiver em zero ou o numero digitado for maior
+        numeros.append(numero) # que o ultimo elemento da lista adicione o novo elemento no final da lista.
+        print('Adicionado ao final da lista... ')
+    else:
+        pos = 0
+        while pos < len(numeros): # enquanto a posição for menor que a quantidade de elementos na lista.
+            if numero <= numeros [pos]: # condição de filtro para verificar se o numero que vou inserir
+                 # na lista é menor ou igual oque já está dentro na posição atual que está o contador.
+                numeros.insert(pos, numero) #caso verdadeiro vou inserir o elemento na posição que está o contador.
+                print(f'Adicionado na posição {pos} da lista')
+                break
+            pos += 1 # passo o contador para a proxima posição.
+
+print('-=' *30 )
+print (f'Os valores digitados em ordem são {numeros}')
 
